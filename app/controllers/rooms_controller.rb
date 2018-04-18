278a9f17953c8878f86746699df53867fb09248e
tsubonet <tsubonet@gmail.com>
2018-04-18 15:08:23 +0900

    delete hello world

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   def show
     @messages = Message.all
+    @hello_world_props = { name: "Stranger" }
   end
 end
---
+++ app/javascript/packs/entry.js
@@ -0,0 +1,8 @@
+import ReactOnRails from "react-on-rails";
+
+import ChatRoom from "../bundles/chat_room";
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  ChatRoom
+});
