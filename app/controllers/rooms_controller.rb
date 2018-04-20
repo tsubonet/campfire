4d1fab635e8726a78992d4f3e56e5016018b9aef
tsubonet <tsubonet@gmail.com>
2018-04-20 14:18:45 +0900

    add chatroom

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,5 +7,10 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @messages = @room.messages
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @room.as_json(:include => :messages) }
+    end
   end
 end
