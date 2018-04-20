f9541eafd02f12f2967889d93536519150831af7
tsubonet <tsubonet@gmail.com>
2018-04-20 11:20:30 +0900

    add chatroom

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,22 +2,11 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "room_channel"
     #stream_for "room_channel_#{params[:id]}"
-    puts "########################"
-    puts params
     room = Room.find(params[:id])
     stream_for room
-    # RoomChannel.broadcast_to(
-    #   room,
-    #   { content: " joined"}
-    # )
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-  end
-
-  def speak(data)
-    #Message.create! content: data['message']
-    #ActionCable.server.broadcast 'room_channel', message: data['message']
   end
 end
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,18 +1,8 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
   def perform(message, room)
-    puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    puts message
-    puts room
-    RoomChannel.broadcast_to(room,
-      message: ActiveSupport::JSON.decode(MessagesController.render(
-        partial: "messages/message.json.jbuilder",
-        locals: { message: message })))
+    RoomChannel.broadcast_to(room, message: ActiveSupport::JSON.decode(render_message(message)))
   end
-
-  # def perform(message)
-  #   ActionCable.server.broadcast "room_channel_#{message.room_id}", message: ActiveSupport::JSON.decode(render_message(message))
-  # end
 
   private
     def render_message(message)
