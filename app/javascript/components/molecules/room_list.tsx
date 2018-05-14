95530ab97c18f89ef9e4b2640b13e50aca84d80c
tsubonet <tsubonet@gmail.com>
2018-05-14 16:53:31 +0900

    error

--- app/javascript/components/molecules/room_list.tsx
+++ app/javascript/components/molecules/room_list.tsx
@@ -6,18 +6,34 @@ import NavLink from '../atoms/nav_link'
 interface Props {
   items: Array<Room>
 }
-const RoomList = ({ items }: Props) => {
-  return (
-    <Root>
-      {items.map((room, idx) => {
-        return (
-          <Li key={idx}>
-            <NavLink to={`/rooms/${room.id}`} label={room.name} />
-          </Li>
-        )
-      })}
-    </Root>
-  )
+
+class RoomList extends React.Component<Props> {
+  private RoomListRef
+  constructor(props) {
+    super(props)
+    this.RoomListRef = React.createRef()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.items !== this.props.items) {
+      this.RoomListRef.current.scrollTop = 0
+    }
+  }
+
+  render() {
+    const { items } = this.props
+    return (
+      <Root innerRef={this.RoomListRef}>
+        {items.map((room, idx) => {
+          return (
+            <Li key={idx}>
+              <NavLink to={`/rooms/${room.id}`} label={room.name} />
+            </Li>
+          )
+        })}
+      </Root>
+    )
+  }
 }
 
 export default RoomList
