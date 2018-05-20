c8290084812d55e2b7b1dbcee2e468cfc93fff95
tsubonet <tsubonet@gmail.com>
2018-05-20 13:20:04 +0900

    error

--- app/javascript/components/pages/edit_room_page.stories.js
+++ app/javascript/components/pages/edit_room_page.stories.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
+import EditRoomPage from './edit_room_page'
 import configureStore from '../../store'
-import NavLink from './nav_link'
 import '../utils/global_style'
 
+const props = {
+  rooms: {
+    items: [
+      {
+        id: 1,
+        name: 'room1',
+      },
+      {
+        id: 2,
+        name: 'room2',
+      },
+    ],
+  },
+}
 const history = createHistory()
-const store = configureStore()
+const store = configureStore(props)
 
-storiesOf('Molecules/NavLink', module)
+storiesOf('Pages/EditRoomPage', module)
   .addDecorator(story => (
     <Provider store={store}>
       <ConnectedRouter history={history}>{story()}</ConnectedRouter>
     </Provider>
   ))
-  .add('デフォルト', () => (
-    <NavLink to={`/rooms/1`} label="デフォルト" destroyRoomAsync={action('onclick')} />
-  ))
+  .add('デフォルト', () => <EditRoomPage />)
