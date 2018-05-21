b2de9770487fbf2b00c324001553da545f61277e
tsubonet <tsubonet@gmail.com>
2018-05-21 14:52:18 +0900

    test

---
+++ app/javascript/test/first.spec.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+
+class Sample extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    const { onWillMountHandle } = this.props
+    onWillMountHandle()
+  }
+
+  render() {
+    const { onClickHandle } = this.props
+    return (
+      <div>
+        <span onClick={onClickHandle}>aaaa</span>
+      </div>
+    )
+  }
+}
+
+describe('toHaveBeenCalled example', () => {
+  let testMock1
+  let testMock2
+  let subject
+
+  beforeEach(() => {
+    testMock1 = jest.fn()
+    testMock2 = jest.fn()
+    subject = shallow(<Sample onWillMountHandle={testMock1} onClickHandle={testMock2} />)
+  })
+
+  test('handle onWillMountHandle', () => {
+    expect(testMock1).toHaveBeenCalled()
+  })
+
+  test('handle onClickHandle', () => {
+    subject.find('span').simulate('click')
+    expect(testMock2).toBeCalled()
+  })
+})
