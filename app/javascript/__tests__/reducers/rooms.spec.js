e262287cd6efd77fe185bc67c018ef7fd631efdf
tsubonet <tsubonet@gmail.com>
2018-05-21 16:56:48 +0900

    test

---
+++ app/javascript/__tests__/reducers/rooms.spec.js
@@ -0,0 +1,25 @@
+import reducer from '../../modules/rooms.ts'
+
+// const initialState = {
+//   year: new Date().getFullYear(),
+//   month: new Date().getMonth() + 1,
+//   date: new Date().getDate(),
+// }
+
+// describe('date reducer', () => {
+//   it('should have initial state', () => {
+//     expect(reducer(undefined, {})).toEqual(initialState)
+//   })
+
+//   it('should handle GET_DATE', () => {
+//     expect(
+//       reducer(
+//         {},
+//         {
+//           type: 'GET_DATE',
+//           date: { year: 2018, month: 1, day: 1 },
+//         }
+//       )
+//     ).toEqual({ year: 2018, month: 1, day: 1 })
+//   })
+// })
