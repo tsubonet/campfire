2c807afc6233ee6375d2762002e1e4448216b026
tsubonet <tsubonet@gmail.com>
2018-05-23 16:01:17 +0900

    test

--- app/javascript/components/molecules/nav_link.tsx
+++ app/javascript/components/molecules/nav_link.tsx
@@ -15,7 +15,7 @@ const Enhance = compose(
     destroyRoom: props => event => {
       event.preventDefault()
       event.stopPropagation()
-      props.destroyRoomAsync(parseInt(props.to.replace('/rooms/', ''), 10))
+      props.destroyRoomAsync(parseInt(props.to.replace('/roomss/', ''), 10))
     },
   })
 )
